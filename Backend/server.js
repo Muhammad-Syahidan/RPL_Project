64b3bbf03d9921ab46e91a9e69cb5e6c94651ef9
MasterNadz <zidan03102006@gmail.com>
2026-05-07 20:35:59 +0800

    Menambah logic di Contoller dan membuat routes untuk memproses data yang dikirim

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const db = require('./config/db'); 
+const { createUserTable } = require('./models/User');
 
-// Import Model
-const { createUserTable } = require('./models/User'); // <-- Tambahkan ini
+// 1. Import file routes yang baru kita buat
+const authRoutes = require('./routes/authRoutes'); // <-- Tambahkan ini
 
 const app = express();
 const PORT = 5000;
@@ -12,8 +13,10 @@ const PORT = 5000;
 app.use(cors());
 app.use(express.json());
 
-// Jalankan pembuatan tabel
-createUserTable(); // <-- Dan panggil fungsinya di sini
+createUserTable();
+
+// 2. Aktifkan routes tersebut
+app.use('/api/auth', authRoutes); // <-- Tambahkan ini
 
 app.get('/api/test', (req, res) => {
   res.json({ message: "Halo! Server backend Shan's Cake sudah berhasil berjalan!" });
