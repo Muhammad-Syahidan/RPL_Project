7a804c42dd6950107ef31c9b0ed82ff70e96ea1c
MasterNadz <zidan03102006@gmail.com>
2026-05-13 21:58:28 +0800

    a

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,36 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import semua halaman yang sudah kita buat
+// (Pastikan letak foldernosya sesuai dengan struktur proyek Anda)
+import LandingPage from './pages/LandingPage';
+import LoginPages from './pages/LoginPages';
 import MenuPage from './pages/MenuPage';
+import CheckoutPage from './pages/CheckoutPage';
+import AdminMenuPage from './pages/AdminMenuPage';
+
 function App() {
   return (
-    <div>
-      <MenuPage />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          {/* Jalur 1: Halaman Pembuka / Sambutan */}
+          <Route path="/" element={<LandingPage />} />
+
+          {/* Jalur 2: Halaman Login / Daftar */}
+          <Route path="/login" element={<LoginPages />} />
+
+          {/* Jalur 3: Halaman Katalog Menu */}
+          <Route path="/menu" element={<MenuPage />} />
+
+          {/* Jalur 4: Halaman Pembayaran */}
+          <Route path="/checkout" element={<CheckoutPage />} />
+
+          {/* Jalur 5: Halaman Khusus Admin (Sembunyi dari pelanggan) */}
+          <Route path="/admin" element={<AdminMenuPage />} />
+          
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
